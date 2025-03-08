690ddcce00340136401179ae22918c4f273967da
Mykhailo D <mykhailo.d@curie-team.com>
2025-03-08 03:21:44 +0200

    first commit

---
+++ AdvicerCV/AdvicerCVApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvicerCVApp.swift
+//  AdvicerCV
+//
+//  Created by Mykhailo Dovhyi on 08.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct AdvicerCVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
