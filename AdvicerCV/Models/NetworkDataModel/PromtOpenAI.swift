89a8f24c759c87e4f09f325da45d31644d3aab6c
Misha Dovhiy <hi@mishadovhiy.com>
2025-04-01 00:37:11 +0300

    prompt

--- AdvicerCV/Models/NetworkDataModel/PromtOpenAI.swift
+++ AdvicerCV/Models/NetworkDataModel/PromtOpenAI.swift
@@ -13,10 +13,11 @@ enum PromtOpenAI {
     var promt:String {
         switch self {
         case .advice(let advice):
-            //what to improve (skills, what skills to add), formating(is skills formating is ok or can be improved)
-            //
+            let properties = PromtOpenAI.Advice.Keys.allCases.compactMap { key in
+                "<\(key.identifier)>(\(key.valueDescription))</\(key.identifier)>"
+            }.joined()
             return """
-generate advice for iOS Developer CV, skills:\(advice.skills ?? "")
+generate advice for iOS Developer CV, skills:\(advice.skills ?? ""), my top skills:SwiftUI,UIKit, optionaly with WatchKit but i like it, desireble job duties:iOS application development,in a cross plutform team. Structure advice response in keys: \(properties)
 """
         }
     }
@@ -31,5 +32,37 @@ generate advice for iOS Developer CV, skills:\(advice.skills ?? "")
 extension PromtOpenAI {
     struct Advice {
         let skills:String?
+        
+        enum Keys:String, CaseIterable, ResponseKeys {
+            case cvCompletnessGrade
+            case skillsGrade
+            case skillImprovmentSuggestion
+            case generalAdvice
+            case skillContentCompletness
+            var valueDescription: String {
+                switch self {
+                case .skillContentCompletness:
+                    "Grade how clear i described skills section"
+                case .generalAdvice:
+                    "General advice"
+                case .cvCompletnessGrade:
+                    "grade cv content completness"
+                case .skillsGrade:
+                    "grade how skills are relevant for the position and years of experience"
+                case .skillImprovmentSuggestion:
+                    "suggest what skills I need to add"
+                }
+            }
+            
+            var identifier: String {
+                self.rawValue
+            }
+        }
+    }
+    
+    protocol ResponseKeys:CaseIterable {
+        /// describes response for request
+        var valueDescription:String { get }
+        var identifier:String { get }
     }
 }
---
+++ AdvicerCV/Service/DB.swift
@@ -0,0 +1,10 @@
+//
+//  DB.swift
+//  AdvicerCV
+//
+//  Created by Mykhailo Dovhyi on 01.04.2025.
+//
+
+import Foundation
+
+
